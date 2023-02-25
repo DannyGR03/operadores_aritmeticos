9e90ceb51faa25d5ffa5fef20b8f7f07fc86d9fc
DannyGR03 <DannyGR03@users.noreply.github.com>
2023-02-25 08:02:34 -0500

    se agrego diagrama de flujo

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -1,1 +1,18 @@
-# programa para implementar los probelmas aritmeticos en python
+# Programa para implementar operadores aritméticos 
+
+
+
+
+# input
+x = int(input("digite el valor de x: "))
+y = int(input("digite el valor de y: "))
+
+
+s = x + y
+r = x - y
+m = x * y
+d = x / y
+mod = x % y
+de = x // y 
+p = x ** y
+
